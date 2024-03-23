b46053e97d5cdd94037475e77293e0be46d13313
hamada ali <133247351+Hamadaalialissawi@users.noreply.github.com>
2024-03-23 14:42:22 -0700

    Adding a new file

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+add: function that adds two integers and returns the result.
+
+@a: first argument
+@b: second argument
+Return: sum of a + b
+"""
+
+
+def add(a, b):
+    sum = a + b
+    return sum
