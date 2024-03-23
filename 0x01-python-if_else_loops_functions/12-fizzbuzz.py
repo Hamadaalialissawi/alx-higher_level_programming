1c5b0d4c90d89387ecfd2d0bf31dda014de7bad4
hamada ali <133247351+Hamadaalialissawi@users.noreply.github.com>
2024-03-23 15:32:30 -0700

    Adding a new file

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -9,8 +9,7 @@ def fizzbuzz():
         if (i % 3) == 0 and i % 5 != 0:
             print("Fizz ", end='')
         elif i % 5 == 0 and i % 3 != 0:
-            if i == 100:
-                print("Buzz ", end='')
+            print("Buzz ", end='')
         elif i % 5 == 0 and i % 3 == 0:
             print("FizzBuzz ", end='')
         else:
