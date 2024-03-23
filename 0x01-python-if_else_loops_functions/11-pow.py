c5d83b4465fe8bbb4bdb8e841838c87537ddd46f
hamada ali <133247351+Hamadaalialissawi@users.noreply.github.com>
2024-03-23 14:51:37 -0700

    Adding a new file

---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+pow: function that computes a to the power of b and return the value.
+
+@a: first argument
+@b: second argument
+Return: sum of a**b
+"""
+
+
+def pow(a, b):
+    sum = a** b
+    return sum
